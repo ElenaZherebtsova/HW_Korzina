f5808e106283b40b3a56a721ee8cc4548bd00ff7
Elena <lenulka.90@mail.ru>
2023-09-10 16:57:02 +0300

    Start.

---
+++ src/main/java/pro/sky/HWKorzina/HwKorzinaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HWKorzina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwKorzinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwKorzinaApplication.class, args);
+	}
+
+}
